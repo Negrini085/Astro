6aad609af4a14aeff8425a927382b8d4bd46dca7
Negrini085 <filippo.negrini@studenti.unimi.it>
2025-10-10 22:38:14 +0200

    Sistemate alcune cose in inizializzazione

--- evo.h
+++ evo.h
@@ -11,18 +11,13 @@ using namespace std;
 
 
 // Funzione per calcolare la distanza fra due particelle
-vector<vector<double>> GetD2(vector<Particella> conf){
-    double x1, x2, y1, y2, dist2;
-    vector<vector<double>> appo = vector<vector<double>>(conf.size(), vector<double>(conf.size(), 0.0));
+vector<vector<double>> GetD2(vector<double> x, vector<double> y){
+    double dist2;
+    vector<vector<double>> appo = vector<vector<double>>(x.size(), vector<double>(x.size(), 0.0));
     
-    for(int i=0; i<conf.size()-1; i++){
-        for(int j=i+1; j<conf.size(); j++){
-            x1 = conf[i].GetR()*cos(conf[i].GetT());
-            x2 = conf[j].GetR()*cos(conf[j].GetT());
-            y1 = conf[i].GetR()*sin(conf[i].GetT());
-            y2 = conf[j].GetR()*sin(conf[j].GetT());
-
-            dist2 = pow((x2 - x1), 2) + pow((y2 - y1), 2);
+    for(int i=0; i<x.size()-1; i++){
+        for(int j=i+1; j<x.size(); j++){
+            dist2 = pow((x[i] - x[j]), 2) + pow((y[i] - y[j]), 2);
             appo[i][j] = dist2;
             appo[j][i] = dist2;
         }
@@ -32,20 +27,47 @@ vector<vector<double>> GetD2(vector<Particella> conf){
 }
 
 
+void GetD2(vector<vector<double>> &appo, vector<double> x, vector<double> y){
+    double dist2;
+    
+    for(int i=0; i<x.size()-1; i++){
+        for(int j=i+1; j<x.size(); j++){
+            dist2 = pow((x[i] - x[j]), 2) + pow((y[i] - y[j]), 2);
+            appo[i][j] = dist2;
+            appo[j][i] = dist2;
+        }
+    }
+}
+
 
 class Sistema{
     public:
         Sistema(CondIn &condin){
-            conf = condin.GetS();
-            dist = GetD2(conf);
+            vector<Particella> conf = condin.GetS();
+
+            for(int i=0; i<conf.size(); i++){
+                x.push_back(conf[i].GetR()*cos(conf[i].GetT()));
+                y.push_back(conf[i].GetR()*sin(conf[i].GetT()));
+                vx.push_back(conf[i].GetVr()*cos(conf[i].GetT()) - conf[i].GetVt()*sin(conf[i].GetT()));
+                vy.push_back(conf[i].GetVr()*sin(conf[i].GetT()) + conf[i].GetVt()*cos(conf[i].GetT()));
+            }
+            dist = GetD2(x, y);
         }
+
+        void evolvi(){
+            vector<double> pos
+        }
+        
+
 
 
 
 
     protected:
-        vector<Particella> conf;
-        vector<vector<double>> dist;
+        vector<double> x, xold, y, yold;
+        vector<double> vx, vy;
+
+        vector<vector<double>> dist, old;
 
 };
 
