1dd60247317dd8fbc6c68976672edd1e725a5c62
Negrini085 <filippo.negrini@studenti.unimi.it>
2025-10-10 23:14:41 +0200

    Pronti per simulare

--- evo.h
+++ evo.h
@@ -2,6 +2,8 @@
 #define __evo__
 
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <cmath>
 
 #include </home/filippo/Desktop/CODICINI/Astro/condin.h>
@@ -11,15 +13,15 @@ using namespace std;
 
 
 // Funzione per calcolare la distanza fra due particelle
-vector<vector<double>> GetD2(vector<double> x, vector<double> y){
+vector<vector<double>> GetD2(vector<double> &x, vector<double> &y, double eps=1e-6){
     double dist2;
     vector<vector<double>> appo = vector<vector<double>>(x.size(), vector<double>(x.size(), 0.0));
     
     for(int i=0; i<x.size()-1; i++){
         for(int j=i+1; j<x.size(); j++){
             dist2 = pow((x[i] - x[j]), 2) + pow((y[i] - y[j]), 2);
-            appo[i][j] = dist2;
-            appo[j][i] = dist2;
+            appo[i][j] = dist2 + eps;
+            appo[j][i] = dist2 + eps;
         }
     }
 
@@ -27,37 +29,37 @@ vector<vector<double>> GetD2(vector<double> x, vector<double> y){
 }
 
 
-void GetD2(vector<vector<double>> &appo, vector<double> x, vector<double> y){
+void GetD2(vector<vector<double>> &appo, vector<double> &x, vector<double> &y, double eps=1e-6){
     double dist2;
     
     for(int i=0; i<x.size()-1; i++){
         for(int j=i+1; j<x.size(); j++){
             dist2 = pow((x[i] - x[j]), 2) + pow((y[i] - y[j]), 2);
-            appo[i][j] = dist2;
-            appo[j][i] = dist2;
+            appo[i][j] = dist2 + eps;
+            appo[j][i] = dist2 + eps;
         }
     }
 }
 
 
-double forceX(vector<vector<double>> &appo, vector<double> x, int ind){
+double forceX(vector<vector<double>> &appo, vector<double> &x, int ind){
     double force = 0;
     
     for(int i=0; i<x.size(); i++){ 
         if(i == ind){ continue; }
-        force += (x[i] - x[ind])/pow((appo[ind][i] + pow(10, -6)), 1.5); 
+        force += (x[i] - x[ind])/pow((appo[ind][i]), 1.5); 
     }
 
     return force;
 }
 
 
-double forceY(vector<vector<double>> &appo, vector<double> y, int ind){
+double forceY(vector<vector<double>> &appo, vector<double> &y, int ind){
     double force = 0;
     
     for(int i=0; i<y.size(); i++){ 
         if(i == ind){ continue; }
-        force += (y[i] - y[ind])/pow((appo[ind][i] + pow(10, -6)), 1.5); 
+        force += (y[i] - y[ind])/pow((appo[ind][i]), 1.5); 
     }
 
     return force;
@@ -85,8 +87,33 @@ class Sistema{
             yold = y;
             for(int i=0; i<x.size(); i++){
                 x[i] = xold[i] + vx[i] * dt + forceX(dist, xold, i) * pow(dt, 2)/2; 
-                x[i] = xold[i] + vx[i] * dt + forceY(dist, yold, i) * pow(dt, 2)/2; 
+                y[i] = yold[i] + vy[i] * dt + forceY(dist, yold, i) * pow(dt, 2)/2; 
             }
+            
+            oldist = dist;
+            GetD2(dist, x, y);
+            for(int i=0; i<x.size(); i++){
+                vx[i] += (forceX(dist, x, i) + forceX(oldist, xold, i)) * dt/2;
+                vy[i] += (forceY(dist, y, i) + forceY(oldist, yold, i)) * dt/2;
+            }
+        }
+
+        void simula(double dt, int nstep){
+            for(int i=0; i<nstep; i++){
+                cout << "Passo: " << i+1 << endl;
+                evolvi(dt);
+                if(nstep%100 == 0 && nstep != 0){ stampaConf(i); }
+            }
+        }
+
+        void stampaConf(int ind){
+            ofstream fileout;
+
+            fileout.open("conf" + to_string(ind) + ".conf");
+            for(int i=0; i<x.size(); i++){
+                fileout << x[i] << "  " << y[i] << "  " << vx[i] << "  " << vy[i] << endl;
+            }
+            fileout.close();
         }
         
 
